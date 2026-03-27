7ff8a52614ba844441d9695300844ef5a2d16060
ehtishamali25-hub <ehtishamali217@gmail.com>
2026-03-27 23:52:47 +0500

    update frontend

--- frontend/src/pages/WebsiteLayout.jsx
+++ frontend/src/pages/WebsiteLayout.jsx
@@ -173,7 +173,7 @@ const WebsiteLayout = ({ children }) => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                <span>2503D N Harrison St, Arlington, VA</span>
+                <span>5801 KINGSTOWNE CENTER DRIVE SUIT 120 ALEXANDRIA, VA 22315</span>
               </div>
             </div>
             
