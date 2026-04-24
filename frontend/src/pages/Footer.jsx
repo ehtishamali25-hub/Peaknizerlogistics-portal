e41ca8f00c9d98a0c7093ec3331aeb395932fc4d
ehtishamali25-hub <ehtishamali217@gmail.com>
2026-04-24 22:39:28 +0500

    Update footer contact information

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -99,7 +99,7 @@ const Footer = () => {
             {[
               { icon: 'M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z', text: 'info@peaknizerlogistics.com' },
               { icon: 'M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z', text: '+1 (571) 307-4461' },
-              { icon: 'M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z', text: '5801 KINGSTOWNE CENTER DRIVE, ALEXANDRIA, VA 22315' }
+              { icon: 'M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z', text: '2503D N Harrison St, Arlington, VA 22207' }
             ].map((item, i) => (
               <div key={i} className="flex items-center space-x-3 text-gray-400 group hover:text-orange-400 transition-colors">
                 <div className="p-2 bg-orange-500/10 rounded-xl group-hover:bg-orange-500/20 transition-all">
