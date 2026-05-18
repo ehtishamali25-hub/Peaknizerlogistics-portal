e5e99d73c003aa48e534f0b460d3e8f34d3049be
ehtishamali25-hub <ehtishamali217@gmail.com>
2026-05-19 04:03:50 +0500

    address change

--- frontend/src/pages/PolicyPage.jsx
+++ frontend/src/pages/PolicyPage.jsx
@@ -263,7 +263,7 @@ const PolicyPage = () => {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                       </svg>
-                      <span className="break-words">2503D N Harrison St, Arlington, VA 22207</span>
+                      <span className="break-words">Arlington, VA && Houston, TX</span>
                     </div>
                   </div>
                 </div>
